0372be2b4f5fef32ebb7b8a59bcd0ae5696b69fd
kirillanpilov <anpilov.kirill2002@gmail.com>
2022-11-06 01:11:08 +0300

    Add logging

--- User.py
+++ User.py
@@ -1,21 +1,36 @@
 import socket
+import logging
+import time
 
 
 def user(host='localhost', port=7671):
+    logging.basicConfig(filename='user.log', filemode='w',format='%(filename)s - %(asctime)s - %(message)s', level=logging.INFO)
     try:
         with socket.socket() as s:
-            print(">>> Соединение с сервером")
-            s.connect((host, port))
+            logging.info('Соединение с сервером')
+            print('Соединение с сервером')
+            try:
+                s.connect((host, port))
+            except OSError:
+                logging.info("Соединение не установлено")
+                print("Соединение не установлено")
+
+
+
             while True:
                 inp = input(">>> Input: ")
                 if inp == "exit":
                     s.close()
-                    print(">>> Разрыв соединения с сервером")
+                    logging.info("Разрыв соединения с сервером")
+                    print("Разрыв соединения с сервером")
                     break
-                print(">>> Отправка данных серверу")
+                logging.info("Отправка данных серверу")
+                print("Отправка данных серверу")
                 s.send(inp.encode())
-                print(">>> Прием данных от сервера")
+                logging.info("Прием данных от сервера")
+                print("Прием данных от сервера")
                 data = s.recv(1024)
+                logging.info(f">>> Echoing:  {data.decode()}")
                 print('>>> Echoing: ', data.decode())
 
     except KeyboardInterrupt:
--- server.py
+++ server.py
@@ -1,26 +1,36 @@
 import socket
+import logging
 
 
-def server(host='localhost', port=7670):
+def server(host='localhost', port=7669):
+    logging.basicConfig(filename='server.log', filemode='w',format='%(filename)s - %(asctime)s - %(message)s', level=logging.INFO)
+
     try:
+        logging.info("Запуск сервера")
         print(">>> Запуск сервера")
         with socket.socket() as s:
 
             s.bind((host, port))
+            logging.info("Начало прослушивания порта")
             print(">>> Начало прослушивания порта")
             while True:
                 s.listen(0)
+                logging.info("Подключение клиента")
                 print(">>> Подключение клиента")
                 conn, addr = s.accept()
                 while True:
+                    logging.info("Приём данных у клиента")
                     print(">>> Приём данных у клиента")
                     data = conn.recv(1024)
                     if not data:
                         break
+                    logging.info("Отправка данных клиенту")
                     print(">>> Отправка данных клиенту")
                     conn.send(data)
+                logging.info("Отключение клиента")
                 print(">>> Отключение клиента")
                 conn.close()
+            logging.info("Отключение сервера")
             print(">>> Отключение сервера")
 
     except KeyboardInterrupt:
